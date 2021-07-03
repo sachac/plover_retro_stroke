a878a0da83dc7fa0bfced4e7c0c8a01d09e41e5b
Antonia Siu <mail@antoniasiu.co.uk>
2021-07-04 00:28:02 +0100

    add arguments for separator, before and after text

--- plover_retro_stroke/__init__.py
+++ plover_retro_stroke/__init__.py
@@ -8,6 +8,11 @@ def flatten(x: List[List]) -> List:
     return list(itertools.chain.from_iterable(x))
 
 def retro_stroke(translator: Translator, stroke: Stroke, cmdline: str):
+    args = cmdline.split(",")
+    separator = args[0] or " "
+    before = args[1] or ""
+    after = args[2] or ""
+
     all_translations = translator.get_state().translations
     affected_translation_cnt = len(list(
         itertools.takewhile(
@@ -17,11 +22,11 @@ def retro_stroke(translator: Translator, stroke: Stroke, cmdline: str):
     ))
     affected_translations = all_translations[-(affected_translation_cnt + 1):]
     affected_strokes = flatten([x.strokes for x in affected_translations])
-    result = ' '.join([x.rtfcre for x in reversed(list(itertools.dropwhile(
+    result = separator.join([x.rtfcre for x in reversed(list(itertools.dropwhile(
         lambda x: x == stroke,
         reversed(affected_strokes)
     )))])
-    my_trans = Translation(affected_strokes + [stroke], result)
+    my_trans = Translation(affected_strokes + [stroke], before + result + after)
     my_trans.replaced = affected_translations
     translator.translate_translation(my_trans)
 
